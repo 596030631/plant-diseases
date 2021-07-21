dc696732c4d0707972484f43b68a7a12f4be6490
Administrator <596>
2021-07-21 17:16:28 +0800

    login

--- rubbish/src/main/java/com/shuaijun/rubbish/ui/LoginActivity.kt
+++ rubbish/src/main/java/com/shuaijun/rubbish/ui/LoginActivity.kt
@@ -1,15 +1,18 @@
 package com.shuaijun.rubbish.ui
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.kaopiz.kprogresshud.KProgressHUD
 import com.shuaijun.rubbish.databinding.ActivityLoginBinding
 import com.shuaijun.rubbish.databinding.ItemLoginBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.functions.Consumer
+import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
@@ -50,58 +53,39 @@ class LoginActivity : AppCompatActivity() {
             .setAnimationSpeed(2)
             .setDimAmount(0.5f)
 
+
+        binding.btnEntre.setOnClickListener {
+            loading.show()
+            Single.timer(1300, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.single())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(Consumer {
+                    loading.dismiss()
+                    Intent(this, FullscreenActivity::class.java).apply {
+                        startActivity(this)
+                    }
+                },{
+
+                })
+        }
+
         val list = mutableListOf<ItemContent>()
         list.add(ItemContent(servers[Random.nextInt(servers.size)], true))
-//        binding.recyclerview.layoutManager = LinearLayoutManager(this)
-//
-//        adapter = Adapter(list, { v, p ->
-//            v.server.text = list[p].server
-//            if (list[p].select) {
-//                v.connect.text = "已连接"
-//            } else {
-//                v.connect.text = ""
-//            }
-//            v.root.setOnClickListener {
-//                loading.show()
-//                Single.timer(1500, TimeUnit.MILLISECONDS)
-//                    .map {
-//                        for (i in list.indices) {
-//                            list[i].select = i == p
-//                        }
-//                    }
-//                    .observeOn(AndroidSchedulers.mainThread())
-//                    .subscribe(Consumer {
-//                        loading.dismiss()
-//                        adapter.notifyDataSetChanged()
-//                    })
-//            }
-//        }, { p ->
-//            ViewHolder(
-//                ItemLoginBinding.inflate(
-//                    LayoutInflater.from(p.context), p, false
-//                )
-//            )
-//        })
-//        binding.recyclerview.adapter = adapter
-//        binding.refresh.setOnRefreshListener {
-//            Single.timer(2, TimeUnit.SECONDS)
-//                .map {
-//                    list.clear()
-//                    servers.forEach {
-//                        if (Random.nextBoolean()) list.add(ItemContent(it, false))
-//                    }
-//                }
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(Consumer {
-//                    adapter.notifyDataSetChanged()
-//                    binding.refresh.isRefreshing = false
-//                })
-//        }
+
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onResume() {
+        super.onResume()
+        full()
+    }
 
+    private fun full() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.root).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 
 }
